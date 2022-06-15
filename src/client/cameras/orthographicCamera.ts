788a4ab6f7d4114fb8adab98066519e00e47c1d3
Taylor Schinvelski <taylorbyks@hotmail.com>
2022-06-14 23:22:47 -0300

    feat: create cameras

--- src/client/cameras/orthographicCamera.ts
+++ src/client/cameras/orthographicCamera.ts
@@ -1,11 +1,96 @@
 import * as THREE from 'three'
 import { width, height } from '../const'
 
-export const orthographicCamera = new THREE.OrthographicCamera(
-  width / -2,
-  width / 2,
-  height / 2,
-  height / -2,
-  1,
-  1000,
-)
+interface OrthographicCameraOptions {
+  zoom?: number
+  left?: number
+  right?: number
+  top?: number
+  bottom?: number
+  near?: number
+  far?: number
+}
+
+class OrthographicCamera extends THREE.Camera {
+  zoom: number
+  left: number
+  right: number
+  top: number
+  bottom: number
+  near: number
+  far: number
+  view:
+    | {
+        enabled: boolean
+        fullWidth: number
+        fullHeight: number
+        offsetX: number
+        offsetY: number
+        width: number
+        height: number
+      }
+    | undefined
+
+  constructor({
+    zoom = 1,
+    left = -1,
+    right = 1,
+    top = 1,
+    bottom = -1,
+    near = 0.1,
+    far = 1000,
+  }: OrthographicCameraOptions) {
+    super()
+
+    this.type = 'Camera'
+    this.zoom = zoom
+    this.left = left
+    this.right = right
+    this.top = top
+    this.bottom = bottom
+    this.near = near
+    this.far = far
+
+    this.updateProjectionMatrix()
+  }
+
+  updateProjectionMatrix() {
+    const dx = (this.right - this.left) / (2 * this.zoom)
+    const dy = (this.top - this.bottom) / (2 * this.zoom)
+    const cx = (this.right + this.left) / 2
+    const cy = (this.top + this.bottom) / 2
+
+    let left = cx - dx
+    let right = cx + dx
+    let top = cy + dy
+    let bottom = cy - dy
+
+    if (this.view !== undefined && this.view.enabled) {
+      const scaleW = (this.right - this.left) / this.view.fullWidth / this.zoom
+      const scaleH = (this.top - this.bottom) / this.view.fullHeight / this.zoom
+
+      left += scaleW * this.view.offsetX
+      right = left + scaleW * this.view.width
+      top -= scaleH * this.view.offsetY
+      bottom = top - scaleH * this.view.height
+    }
+
+    this.projectionMatrix.makeOrthographic(
+      left,
+      right,
+      top,
+      bottom,
+      this.near,
+      this.far,
+    )
+
+    this.projectionMatrixInverse.copy(this.projectionMatrix).invert()
+  }
+}
+
+export const orthographicCamera = new OrthographicCamera({
+  left: width / -2,
+  right: width / 2,
+  top: height / 2,
+  bottom: height / -2,
+})
--- src/client/cameras/perspectiveCamera.ts
+++ src/client/cameras/perspectiveCamera.ts
@@ -1,12 +1,89 @@
 import * as THREE from 'three'
+import { MathUtils } from 'three'
 import { width, height } from '../const'
 
-export const perspectiveCamera = new THREE.PerspectiveCamera(
-  75,
-  width / height,
-  0.1,
-  1000,
-)
+interface PerspectiveCameraOptions {
+  fov?: number
+  aspect?: number
+  position?: THREE.Vector3
+}
+class PerspectiveCamera extends THREE.Camera {
+  type: string
+  fov: number
+  zoom: number
+  near: number
+  far: number
+  focus: number
+  aspect: number
+  filmGauge: number
+  filmOffset: number
+  projectionMatrix: any
+  projectionMatrixInverse: any
+  position: THREE.Vector3
+  view:
+    | {
+        enabled: boolean
+        fullWidth: number
+        fullHeight: number
+        offsetX: number
+        offsetY: number
+        width: number
+        height: number
+      }
+    | undefined
 
-perspectiveCamera.position.z = 4
-perspectiveCamera.position.y = 2
+  constructor({
+    fov = 75,
+    aspect = 1,
+    position = new THREE.Vector3(0, 0, 0),
+  }: PerspectiveCameraOptions) {
+    super()
+
+    this.type = 'Camera'
+    this.fov = fov
+    this.zoom = 1
+    this.near = 0.1
+    this.far = 1000
+    this.focus = 10
+    this.aspect = aspect
+    this.filmGauge = 35
+    this.filmOffset = 0
+    this.position = position
+
+    this.updateProjectionMatrix()
+  }
+
+  updateProjectionMatrix() {
+    const near = this.near
+    let top = (near * Math.tan(MathUtils.DEG2RAD * 0.5 * this.fov)) / this.zoom
+    let height = 2 * top
+    let width = this.aspect * height
+    let left = -0.5 * width
+
+    if (this.view !== undefined && this.view.enabled) {
+      const fullWidth = this.view.fullWidth,
+        fullHeight = this.view.fullHeight
+
+      left += (this.view.offsetX * width) / fullWidth
+      top -= (this.view.offsetY * height) / fullHeight
+      width *= this.view.width / fullWidth
+      height *= this.view.height / fullHeight
+    }
+
+    this.projectionMatrix.makePerspective(
+      left,
+      left + width,
+      top,
+      top - height,
+      near,
+      this.far,
+    )
+
+    this.projectionMatrixInverse.copy(this.projectionMatrix).invert()
+  }
+}
+
+export const perspectiveCamera = new PerspectiveCamera({
+  aspect: width / height,
+  position: new THREE.Vector3(4, 2, 0),
+})
